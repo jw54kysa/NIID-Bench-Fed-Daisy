b21b760cf98efa5ecd78c2dc89802a316786cbb7
jw54kysa <jonathan.weske@gmail.com>
2025-01-21 12:25:18 +0100

    lets go

--- scripts/create_partitions.sh
+++ scripts/create_partitions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --
-#SBATCH --job-name=niid-bench-feddc
+#SBATCH --job-name=create_niid_partitions
 #SBATCH --partition=paula
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a30:4
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
 #SBATCH --time=2-00:00:00
 #SBATCH -o log/%x.out-%j
@@ -13,9 +13,9 @@
 
 for dataset in cifar10
 do
-  for n_parties in 10 50 100 150
+  for part in homo iid-diff-quantity noniid-labeldir real mixed
   do
-    for part in homo iid-diff-quantity
+    for n_parties in 10 50 100 150
     do
       python create_partitions.py \
         --dataset=$dataset \
