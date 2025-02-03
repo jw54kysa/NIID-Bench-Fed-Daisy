7759ecd014885301fabd7e87fcd8817e0336a8e5
jw54kysa <jonathan.weske@gmail.com>
2025-02-03 11:19:17 +0100

    fix visits plot

--- plots.py
+++ plots.py
@@ -4,13 +4,48 @@ import numpy as np
 from collections import Counter
 from utils import *
 
-# Plot random size client samples
-def plot_rss(client_idxs, visits, path, args):
-    data = {}
+# PLOT SAMPLE SIZE AND VISITS
+def plot_rss_visits(client_idxs, visits, path):
+
     counts = [0 for _ in range(len(client_idxs))]
     for idx, l in client_idxs.items():
         counts[idx] = len(l)
 
+    fig, ax1 = plt.subplots(figsize=(16, 8))
+
+    if visits is not None:
+        combined = sorted(zip(counts, visits.values()), key=lambda x: x[0])
+        sorted_counts, sorted_visits = zip(*combined)
+
+        counts = list(sorted_counts)
+        visits = list(sorted_visits)
+
+    # Bar plot for sample size
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax1.bar(np.arange(len(counts)), counts, label='Sample Size', alpha=0.7, color='blue')
+    ax1.set_title("Client Sample Size")
+    ax1.set_xlabel("Client")
+    ax1.set_ylabel("Sample Size", color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
+
+    if visits is not None:
+        # Create second y-axis for visits
+        ax2 = ax1.twinx()
+        ax2.plot(np.arange(len(visits)), visits, label='Visits', color='red', marker='o')
+        ax2.set_ylabel("Visits", color='red')
+        ax2.tick_params(axis='y', labelcolor='red')
+
+    # Adding legends for both plots
+    fig.legend()
+
+    # Save and display the plot
+    plt.savefig(path + "/rss_plt.png")
+
+# Plot random size client samples
+def plot_data_dis(client_idxs, path, args):
+
+    data = {}
+    for idx, l in client_idxs.items():
         train_dl_local, test_dl_local, _, _ = get_dataloader(args.dataset, args.datadir, args.batch_size, 32, l,
                                                              0)
         label_counter = Counter()
@@ -41,38 +76,6 @@ def plot_rss(client_idxs, visits, path, args):
     ax.legend()
 
     plt.savefig(path + "/rss_plt_niid.png")
-
-    # PLOT SAMPLE SIZE AND VISITS
-
-    fig, ax1 = plt.subplots(figsize=(16, 8))
-
-    if visits is not None:
-        combined = sorted(zip(counts, visits), key=lambda x: x[0])
-        sorted_counts, sorted_visits = zip(*combined)
-
-        counts = list(sorted_counts)
-        visits = list(sorted_visits)
-
-    # Bar plot for sample size
-    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax1.bar(np.arange(len(counts)), counts, label='Sample Size', alpha=0.7, color='blue')
-    ax1.set_title("Client Sample Size")
-    ax1.set_xlabel("Client")
-    ax1.set_ylabel("Sample Size", color='blue')
-    ax1.tick_params(axis='y', labelcolor='blue')
-
-    if visits is not None:
-        # Create second y-axis for visits
-        ax2 = ax1.twinx()
-        ax2.plot(np.arange(len(visits)), visits, label='Visits', color='red', marker='o')
-        ax2.set_ylabel("Visits", color='red')
-        ax2.tick_params(axis='y', labelcolor='red')
-
-    # Adding legends for both plots
-    fig.legend()
-
-    # Save and display the plot
-    plt.savefig(path + "/rss_plt.png")
 
 
 def calculate_label_distribution(client_idxs, path, args):
