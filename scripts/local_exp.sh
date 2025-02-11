a14b51fbb95ed1aa0287e5c2f37513a84c89f3d2
jw54kysa <jonathan.weske@gmail.com>
2025-02-11 10:57:42 +0100

    restart canceled exp

--- scripts/local_exp.sh
+++ scripts/local_exp.sh
@@ -2,24 +2,24 @@
 for perm in prob_size
 do
   python -u experiments.py \
-      --model=simple-cnn \
-      --dataset=mnist \
-      --alg=feddc \
-      --lr=0.01 \
-      --batch-size=64 \
-      --epochs=10 \
-      --n_parties=10 \
-      --rho=0.9 \
-      --mu=0.01 \
-      --comm_round=10 \
-      --daisy=0 \
-      --daisy_perm=$perm \
-      --partition='iid-diff-quantity' \
-      --beta=0.5 \
-      --device='mps' \
-      --datadir='./data/' \
-      --logdir='./logs/' \
-      --noise=0 \
-      --sample=1 \
-      --init_seed=0
+    --model=vgg-9 \
+    --dataset=cifar10 \
+    --alg=feddc \
+    --lr=0.01 \
+    --batch-size=32 \
+    --epochs=10 \
+    --n_parties=100 \
+    --rho=0.9 \
+    --mu=0.01 \
+    --comm_round=75 \
+    --daisy=10 \
+    --daisy_perm=$perm \
+    --partition=iid-diff-quantity-rand-sb \
+    --beta=0.5 \
+    --device='cpu' \
+    --datadir='./data/' \
+    --logdir='./logs/' \
+    --noise=0 \
+    --sample=1 \
+    --init_seed=1
 done
