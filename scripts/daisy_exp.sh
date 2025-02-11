838c243679d383e7bbcca1422f6f1b5e42d963ca
jw54kysa <jonathan.weske@gmail.com>
2025-02-11 10:57:42 +0100

    restart canceled exp

--- scripts/daisy_exp.sh
+++ scripts/daisy_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --
-#SBATCH --job-name=ssds_test
-#SBATCH --partition=paul
+#SBATCH --job-name=vgg9_test
+#SBATCH --partition=paula
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --mem=128G
@@ -13,7 +13,7 @@ for alg in feddc
 do
   for perm in rand #prob_size
   do
-    srun singularity exec --nv FEDDC.sif \
+    srun singularity exec FEDDC.sif \
     python3.9 -u experiments.py \
       --model=vgg-9 \
       --dataset=cifar10 \
@@ -21,10 +21,10 @@ do
       --lr=0.01 \
       --batch-size=32 \
       --epochs=10 \
-      --n_parties=100 \
+      --n_parties=50 \
       --rho=0.9 \
       --mu=0.01 \
-      --comm_round=75 \
+      --comm_round=50 \
       --daisy=10 \
       --daisy_perm=$perm \
       --partition=iid-diff-quantity-rand-sb \
