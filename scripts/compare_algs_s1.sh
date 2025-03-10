55c639b6312cd5b5ab5428905a0f2cf1c0367b0b
jw54kysa <jonathan.weske@gmail.com>
2025-03-10 09:58:28 +0100

    new compare

--- scripts/compare_algs_s1.sh
+++ scripts/compare_algs_s1.sh
@@ -15,17 +15,19 @@ do
   python3.9 -u experiments.py \
     --model=simple-cnn \
     --dataset=cifar10 \
+    --partition_path='results_s1/cifar10/iid-diff-quantity/feddc/prob_size/simple-cnn/experiment-2025-02-23-20:27-16' \
     --alg=$alg \
     --lr=0.01 \
     --batch-size=64 \
     --epochs=10 \
-    --n_parties=100 \
+    --n_parties=50 \
     --rho=0.9 \
     --mu=0.01 \
     --comm_round=25 \
     --daisy=10 \
     --daisy_perm=prob_size \
-    --partition=iid-diff-quantity-rand \
+    --partition=iid-diff-quantity \
+    --partition_path='results_s1/cifar10/iid-diff-quantity/fedavg/simple-cnn/experiment-2025-02-21-17\:44-37/partition_tuple.pkl' \
     --beta=0.5 \
     --device='cpu' \
     --datadir='./data/' \
