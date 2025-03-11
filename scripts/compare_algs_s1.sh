9f42703a0c1b44fabe43433d3769890a11faa4b0
jw54kysa <jonathan.weske@gmail.com>
2025-03-11 09:31:32 +0100

    start new compare s2

--- scripts/compare_algs_s1.sh
+++ scripts/compare_algs_s1.sh
@@ -9,25 +9,23 @@
 #SBATCH -e log/%x.error-%j
 #SBATCH --mail-type=END
 
-for alg in feddc
+for alg in fedavg #fedprox/scaffold/fednova/feddc
 do
   srun singularity exec FEDDC.sif \
   python3.9 -u experiments.py \
     --model=simple-cnn \
     --dataset=cifar10 \
-    --partition_path='results_s1/cifar10/iid-diff-quantity/feddc/prob_size/simple-cnn/experiment-2025-02-23-20:27-16' \
     --alg=$alg \
     --lr=0.01 \
-    --batch-size=64 \
+    --batch-size=32 \
     --epochs=10 \
-    --n_parties=50 \
+    --n_parties=100 \
     --rho=0.9 \
     --mu=0.01 \
-    --comm_round=25 \
+    --comm_round=50 \
     --daisy=10 \
-    --daisy_perm=prob_size \
-    --partition=iid-diff-quantity \
-    --partition_path='results_s1/cifar10/iid-diff-quantity/fedavg/simple-cnn/experiment-2025-02-21-17\:44-37/partition_tuple.pkl' \
+    --daisy_perm=rand \
+    --partition=iid-diff-quantity-rand-sb \
     --beta=0.5 \
     --device='cpu' \
     --datadir='./data/' \
