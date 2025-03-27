0361db91669bddf25e802b69a4e08ca12ae1b542
jw54kysa <jonathan.weske@gmail.com>
2025-03-27 16:01:44 +0100

    start 2 run

--- scripts/compare_algs.sh
+++ scripts/compare_algs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash --
 #SBATCH --job-name=comp_long
-#SBATCH --partition=paul
+#SBATCH --partition=paul-long
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --mem=128G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH -o log/%x.out-%j
 #SBATCH -e log/%x.error-%j
 #SBATCH --mail-type=END
 
-for alg in fedavg
+for alg in feddc
 do
   srun singularity exec FEDDC.sif \
   python3.9 -u experiments.py \
     --model=simple-cnn \
     --dataset=cifar10 \
-    --nets_path='results_long/cifar10/iid-diff-quantity-rand/fedavg/simple-cnn/experiment-2025-03-20-20:21-58/nets.pkl' \
+    --nets_path='results_long/cifar10/iid-diff-quantity-rand/feddc/rand/simple-cnn/experiment-2025-03-24-09:34-08/nets.pkl' \
     --alg=$alg \
     --lr=0.01 \
     --batch-size=16 \
@@ -23,9 +23,9 @@ do
     --n_parties=200 \
     --rho=0.9 \
     --mu=0.01 \
-    --comm_round=100 \
+    --comm_round=50 \
     --daisy=10 \
-    --daisy_perm=prob_size \
+    --daisy_perm=rand \
     --partition=iid-diff-quantity-rand \
     --beta=0.5 \
     --device='cpu' \
